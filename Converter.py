55179d12d67e4e0d7a9d2a894f6dbfa355ed3e92
Sigma-UP <62567341+Sigma-UP@users.noreply.github.com>
2020-06-05 13:57:14 +0300

    Add files via upload
    -translated into eng
    -input validations func added (inputMenu && inputMenu)
    -added 5 currencies

--- Converter.py
+++ Converter.py
@@ -1,22 +1,87 @@
 from sys import exit
 
-while True:
-    print("=======================")
-    print("Выберите валюту для конвертации:")
-    print("[0] USD -- > UAH\n[1] UAH --> USD\n[9] Выход из программы.")
-    choice = int(input("Ваш выбор: "))
-    print()
-    if choice == 0 or choice == 1:
-        coeffic = float(input("Введите курс USD в UAH: "))
-        if choice == 0:
-            USD = float(input("Введите количество USD: "))
-            UAH = USD*coeffic
-            print(USD, "USD =", UAH, "UAH")
-        elif choice == 1:
-            UAH = float(input("Введите количество UAH: "))
-            USD = UAH/coeffic
-            print(UAH, " UAH = ", USD, " USD")
-    elif choice == 9:
-        print("Выход из программы...")
-        exit()
-    print("=======================\n")
+def inputFloat(default, invitation = ""):
+	isExcept = True
+	while(isExcept):
+		try:
+			value = input(invitation)
+			if value == "":
+				return default
+			value = float(value)
+			if value < 0:
+				raise Exception("ERROR: Value is less than zero.")
+			if(value == 0):
+				raise Exception("ERROR: Value is zero.")
+		except ValueError:
+			print("ERROR: Invalid data type.")
+		except Exception as CurrentException:
+			print(CurrentException)
+		else:
+			isExcept = False
+	return value
+def inputMenu(default, exitCode = 1488, menuItemsNum = 0, invitation = ""):
+	isExcept = True
+	while(isExcept):
+		try:
+			value = input(invitation)
+			if value == "":
+				return default
+
+			value = int(value)
+			if value == exitCode:
+				exit()
+			if (value < 0 or value >= menuItemsNum) and value != exitCode:
+				raise Exception()
+		except ValueError:
+			print("ERROR: Invalid data type.")
+		except Exception:
+			print("ERROR: Number does not correspond to any menu item.")
+
+		else:
+			isExcept = False
+
+	return value
+
+def swap(list, serialNum):
+	temp = list[0]
+	list[0] = list[serialNum]
+	list[serialNum] = temp
+
+convert = lambda currency, rate: currency*rate
+
+def main():
+	currencyList = ["EUR", "USD", "RUB", "UAH", "PLN", "NOK", "PKR"]
+	rateList = [1, 1, 1, 1, 1, 1, 1]
+
+	while True:
+		print("=======================")
+
+		i = 0
+		while(i < len(currencyList)):
+			print("[", i, "] -- ", currencyList[i], sep="")
+			i+=1
+
+		chosenCurrency = inputMenu(default = 0, exitCode = -1, menuItemsNum = len(currencyList), invitation = "Choose currency or enter -1 to exit: ")
+		if chosenCurrency != 0:
+			swap(currencyList, chosenCurrency)
+		print()
+
+		print("Enter currency rate:")
+		i = 1
+		while(i < len(currencyList)):
+			print(currencyList[0], " 1 = ", currencyList[i], " ", sep = "", end ="")
+			rateList[i] = inputFloat(rateList[i])
+			i+=1
+
+		i = 0
+		print("How much", currencyList[0], " do you need to convert? ", end=" ")
+		originCurrency = inputFloat(rateList[i])
+
+		print("Currency rate table:")
+		i = 1
+		while(i < len(currencyList)):
+			print(currencyList[0], originCurrency, " =", currencyList[i], convert(rateList[i], originCurrency))
+			i+=1
+
+
+main()
